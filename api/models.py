f2f5902f9498b0395b19ec899f32b8b980e2458c
umigit <hamu56Kiki>
2019-04-19 15:48:33 +0900

    make post model

--- api/models.py
+++ api/models.py
@@ -3,3 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=16)
+    name = models.CharField(max_length=32)
+    place = models.CharField(max_length=32)
+    date = models.CharField(max_length=32)
+    detail = models.TextField(default="")
+    address = models.CharField(max_length=256)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    image = models.ImageField(upload_to='image/%Y%m%d')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
