2a53920e657bf954570cf547eee59e76128e2e52
umigit <hamu56Kiki>
2019-04-20 06:19:43 +0900

    debug false

--- tigrashaAPI/settings.py
+++ tigrashaAPI/settings.py
@@ -23,7 +23,7 @@ AUTH_USER_MODEL = 'api.User'
 SECRET_KEY = 'b%g=&(7)ws=s+^bi55%p3ui@ufa(bp%0x5cg%3a1ch39o061x8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
